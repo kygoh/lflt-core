441eb91a249ed6c16518057baf284a5bed4f25e3
Goh Keng Yew <kygoh@myriadeas.com.my>
2020-07-31 11:37:28 +0800

    added services to public api

--- projects/lflt-core/src/public-api.ts
+++ projects/lflt-core/src/public-api.ts
@@ -6,4 +6,5 @@ export * from './lib/components/leaflet-map/leaflet-map.component';
 export * from './lib/directives/leaflet-layer.directive';
 export * from './lib/directives/leaflet-interactive-layer.directive';
 export * from './lib/directives/leaflet-interactive-layers.directive';
+export * from './lib/services/map-facade.service';
 export * from './lib/lflt-core.module';
